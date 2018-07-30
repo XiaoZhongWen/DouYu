3bced9d4ab958aecebdcc6488298030d094046df
肖仲文 <zhongwenxiao@hotmail.com>
2018-07-30 23:27:36 +0800

    搭建UI结构

---
+++ DouYu/DouYu/Classes/Main/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  DouYu
+//
+//  Created by 肖仲文 on 2018/7/30.
+//  Copyright © 2018 肖仲文. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addChildVc(storyboardName: "Home")
+        addChildVc(storyboardName: "Lie")
+        addChildVc(storyboardName: "Follow")
+        addChildVc(storyboardName: "Profile")
+    }
+
+    private func addChildVc(storyboardName : String) {
+        let childVc = UIStoryboard.init(name:storyboardName, bundle: nil).instantiateInitialViewController()!
+        addChildViewController(childVc)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
